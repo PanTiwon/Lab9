b074fc6fcf3516d4a419ed0004574deb0135c945
Tiw1 <kkhowphuns@gmail.com>
2024-12-13 15:58:52 +0700

    lab9_2 Complete

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+int printO(int x,int y){
+	if ( x > 0 && y > 0)
+	{
+	for (size_t i = 0; i < x; i++)
+	{
+		for (int i = 0; i < y; i++)
+		{
+			cout << "O";
+		}
+		cout << "\n";
+	}
+	}else
+	cout << "Invalid input";
+	return 0;
+}
 
 int main(){
 	
